a9fa5f169ad4bf43aec08d426eac203f9366914b
Jose Roberto Correa <jroberto.cr1@gmail.com>
2021-02-05 19:23:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { Button, TextField } from "@material-ui/core";
+import { blue } from "@material-ui/core/colors";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./global.css";
+import Routes from "./Routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Loading, Notify, Alert, Confirm } from "./view/components";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+  },
+  props: {
+    MuiTextField: {
+      variant: "outlined",
+      fullWidth: true,
+    },
+    MuiSelect: {
+      variant: "outlined",
+      fullWidth: true,
+    },
+  },
+});
+
+const App = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Confirm
+        open={false}
+        onClose={() => console.log("close")}
+        onConfirm={() => console.log("confirm")}
+      />
+      <Alert />
+      <Notify />
+      <Loading />
+      <Routes />
+    </ThemeProvider>
+  </Provider>
+);
 
 export default App;
